2e41a16c9605ebe5a8cd1dadb13d0a2edf18c3fa
remadex <s.cardon@outlook.com>
2019-12-16 10:25:55 +0100

    debounce works but call function multiple times

--- app/modifiers/slider-over.js
+++ app/modifiers/slider-over.js
@@ -9,15 +9,41 @@ export default modifier(function sliderOver(element, params/*, params, hash*/) {
   if (element.parentElement.classList.contains("slider-frame")) {
     sliderFrame = element.parentElement;
   }
-  const scaling = 2;
-  
-  console.log(this);
+  const scaling = 2;  
 
   slides.forEach(
     (slide, i) => {
-      slideOverEvent(slide, i);
-      slide.addEventListener("mouseover", slideOverEvent(slide, i))
-      slide.addEventListener("mouseout", slideOutEvent(slide, i))
+      // slideOverEvent(slide, i);
+      slide.addEventListener("mouseover", slideOverEvent)
+      slide.addEventListener("mouseout", slideOutEvent)
+
+      function slideOverEvent(){
+        debounce(function() {
+          console.log("TRIGGER");
+          if (i > 0) {
+            sliderFrame.style.left = `-${slide.offsetWidth / 2}px`;
+          }
+          for (var j = i + 1; j < slides.length; j++) {
+            slides[j].style.left = `${slide.offsetWidth}px`;
+          }
+          slide.style.transform = `scale(${scaling})`;
+          slide.style.left = `${slide.offsetWidth / 2}px`
+        }, 1000)
+      };
+    
+      function slideOutEvent(){
+        debounce(function() {
+          if (i > 0) {
+            sliderFrame.style.left = 0;
+          }
+          for (var j = i + 1; j < slides.length; j++) {
+            slides[j].style.left = 0;
+          }
+          slide.style.transform = "scale(1)";
+          slide.style.left = 0;
+        }, 1000)
+      };
+      
       // slide.addEventListener("mouseout", debounce(context, function () {
       //   if (i > 0) {
       //     sliderFrame.style.left = 0;
@@ -31,31 +57,7 @@ export default modifier(function sliderOver(element, params/*, params, hash*/) {
     }
   );
 
-  function slideOverEvent(slide, i){
-    debounce(myContext, function() {
-      if (i > 0) {
-        sliderFrame.style.left = `-${slide.offsetWidth / 2}px`;
-      }
-      for (var j = i + 1; j < slides.length; j++) {
-        slides[j].style.left = `${slide.offsetWidth}px`;
-      }
-      slide.style.transform = `scale(${scaling})`;
-      slide.style.left = `${slide.offsetWidth / 2}px`
-    }, 1000)
-  };
-
-  function slideOutEvent(slide, i){
-    debounce(myContext, function() {
-      if (i > 0) {
-        sliderFrame.style.left = 0;
-      }
-      for (var j = i + 1; j < slides.length; j++) {
-        slides[j].style.left = 0;
-      }
-      slide.style.transform = "scale(1)";
-      slide.style.left = 0;
-    }, 1000)
-  };
+  
   // function debounce(func, wait, immediate) {
   //   var timeout;
   //   return function () {
